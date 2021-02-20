4afba4a9278c5c500a76882928653afe21235fbd
ninapandya <59859111+ninapandya@users.noreply.github.com>
2021-02-20 10:55:38 -1000

    Added ability to flip between cards. Adjusted the card to have rounded corners

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -8,17 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var card: UIView!
     @IBOutlet weak var backLabel: UILabel!
     @IBOutlet weak var frontLabel: UILabel!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        card.layer.cornerRadius = 20.0
+        card.clipsToBounds = true
     }
 
     @IBAction func didTapOnFlashcard(_ sender: Any) {
-        frontLabel.isHidden = true
+        if frontLabel.isHidden == true{
+            frontLabel.isHidden = false
+        } else {
+            frontLabel.isHidden = true
+        }
+            
     }
     
 }
